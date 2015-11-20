c934626ff60d584477e04e7b8b0578e1b9ce6567
alisonvogel12 <alison.vogel12@gmail.com>
2015-11-19 21:13:40 -0500

    added css bundling for site css

--- AlisonVogel.Web/Global.asax.cs
+++ AlisonVogel.Web/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AlisonVogel.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace AlisonVogel.Web
@@ -13,6 +15,7 @@ namespace AlisonVogel.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
