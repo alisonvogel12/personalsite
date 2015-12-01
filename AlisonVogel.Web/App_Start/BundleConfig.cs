4beff70bca566edbb1c9a5548a16fd6b931c3122
alisonvogel12 <alison.vogel12@gmail.com>
2015-12-01 11:40:01 -0500

    bundle CDN's and rename partial views

--- AlisonVogel.Web/App_Start/BundleConfig.cs
+++ AlisonVogel.Web/App_Start/BundleConfig.cs
@@ -11,10 +11,20 @@ namespace AlisonVogel.Web.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             {
+                bundles.UseCdn = true;
+
                 StyleBundle siteCSS = new StyleBundle("~/Content/siteCSS");
                 siteCSS.Include("~/Content/navbar_style.css",
                   "~/Content/site_style.css");
                 bundles.Add(siteCSS);
+
+                StyleBundle bootstrapCSS = new StyleBundle("~/Content/bootstrapCSS", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css");
+                bootstrapCSS.Include("~/Content/bootstrap.min.css");
+                bundles.Add(bootstrapCSS);
+
+                StyleBundle fontawesomeCSS = new StyleBundle("~/Content/fontawesomeCSS", "//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
+                fontawesomeCSS.Include("~/Content/font-awesome.css");
+                bundles.Add(fontawesomeCSS);
 
                 ScriptBundle siteJS = new ScriptBundle("~/Scripts/siteJS");
                 siteJS.Include("~/Scripts/jquery-2.1.4.min.js", "~/Scripts/bootstrap.min.js");
